f733ae606dcf3a33d51a13762a63f16bc5214291
Rotar_mg75@student.itstep.org <Rotar_mg75@student.itstep.org>
2024-12-20 16:09:54 +0100

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+
+namespace WpfApp1
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            UsernameTextBox.Focus(); // Устанавливаем фокус на текстовое поле имени пользователя
+        }
+
+        private void LogIn_Click(object sender, RoutedEventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordBox.Password;
+            bool rememberMe = RememberMeCheckBox.IsChecked ?? false;
+
+            if (!string.IsNullOrEmpty(username) && !string.IsNullOrEmpty(password))
+            {
+                MessageBox.Show($"Welcome, {username}!", "Authentication Successful", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            else
+            {
+                MessageBox.Show("Please fill in all fields.", "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
